c93130f413f66dc833e1ee4cb47b7239e0ee1aaf
jinyuttt <jinyu20072787@sina.com>
2019-01-09 00:49:51 +0800

    Signed-off-by: jinyuttt <jinyu20072787@sina.com>

---
+++ WPFUIDemo/FloatWin/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FloatWin
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Background = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(TipToolAll))
+            };
+        }
+        /// <summary>
+        /// 窗口拖动
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        void FloatWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = true;
+            this.Left = SystemParameters.WorkArea.Width - 300;
+            this.Top = 300;
+        }
+
+        private static readonly string TipToolAll = "pack://application:,,,/Skin/images/TipToolMainHover.png";
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPFUIDemo/Thunder UI/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//该源码下载自C#编程网|www.cpbcw.com
+namespace Thunder_UI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void x_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
